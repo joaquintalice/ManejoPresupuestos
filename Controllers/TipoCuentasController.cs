25893ec4bba27b010adb44f30da70cad0bd78558
Joaquin Talice <joaquintalice@gmail.com>
2023-06-30 08:43:09 -0300

    Initial commit

---
+++ Controllers/TipoCuentasController.cs
@@ -0,0 +1,24 @@
+﻿using ManejoPresupuestos2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuestos2.Controllers
+{
+    public class TipoCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+﻿# MVC Learn || Backend
